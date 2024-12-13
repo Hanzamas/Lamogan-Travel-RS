fba47072fbf0dac850a9c2ab6acf3c6824883694
Hanzcode <220441100156@student.trunojoyo.ac.id>
2024-12-13 15:37:26 +0700

    first comit

--- main3.py
+++ main3.py
@@ -15,9 +15,9 @@ indonesian_stop_words = factory.get_stop_words()
 # Load datasets
 @st.cache
 def load_data():
-    tourism_rating = pd.read_csv("tourism_rating.csv", encoding="latin1")
-    tourism_with_id = pd.read_csv("tourism_with_id.csv", encoding="latin1")
-    user_data = pd.read_csv("user.csv", encoding="latin1")
+    tourism_rating = pd.read_csv("data/tourism_rating.csv", encoding="Windows-1252")
+    tourism_with_id = pd.read_csv("data/tourism_with_id.csv", encoding="Windows-1252")
+    user_data = pd.read_csv("data/user.csv", encoding="Windows-1252")
     return tourism_rating, tourism_with_id, user_data
 
 tourism_rating, tourism_with_id, user_data = load_data()
